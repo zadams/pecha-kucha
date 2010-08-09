1687f6e7ce0bed4d7ce693fbc005517c16759b38
James Prolizo <jprolizo@pair1.(none)>
2010-08-09 16:27:34 -0400

    [ZA/JP] Adding authentication to application

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml", :version => '3.0.15'
+  config.gem 'warden'
+  config.gem 'devise'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
