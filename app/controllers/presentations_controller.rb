f435a1aafb2d3936a67030e8decc958d814dca04
James Prolizo <jprolizo@pair1.(none)>
2010-08-11 13:36:09 -0400

    [ZA/JP] Presntation edit access

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,9 +1,10 @@
 class PresentationsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :action => [:index, :create]
+  before_filter :authenticate, :action => [:edit, :update]
   
   def index
     @presentation = Presentation.new
-    @events = Event.all(:conditions => ['date >= ?', Time.now], :order => "date ASC")
+    load_events
   end
   
   def create 
@@ -12,6 +13,29 @@ class PresentationsController < ApplicationController
     else
       render :action => :index
     end
+  end
+  
+  def edit
+    @presentation = Presentation.find_by_id(params[:id])
+    load_events
+  end
+  
+  def update
+    if Presentation.update(params[:presentation])
+      render :partial => 'thank_you'
+    else
+      render :action => :edit
+    end
+  end
+  
+  private
+  
+  def authenticate
+    admin_signed_in? || user_signed_in?
+  end
+  
+  def load_events
+    @events = Event.all(:conditions => ['date >= ?', Time.now], :order => "date ASC")
   end
   
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @presentations = Presentation.all(:limit => 3, :conditions => "video_file_size IS NOT NULL")
   end
 end
