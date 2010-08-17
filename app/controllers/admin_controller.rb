9352c855dcdec720540239c66806cb32e5f0e15b
Zack Adams <info@zadams.com>
2010-08-17 14:15:46 -0400

    [ZA] Basic styling + admin improvements

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,9 +2,10 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin!
   
   def index
-    @events = Event.all(:limit => 10)
+    @events = Event.all(:limit => 3)
     @allevents = Event.all
     @event = Event.new
+    @event_locations = Event.all.collect { |e| e.location }
   end
   
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+    if($('.flash_info')){
+      $('.flash_info').animate({opacity: 1.0},2000,function(){ 
+        $('.flash_info').hide('blind','',1000);
+      }); 
+    }
+});
