1328e705d9624da5b0f70cd0a3eee51ed8ebd112
Zack Adams <zack@pair7.(none)>
2010-09-30 11:09:59 -0400

    [ZA/JN] Adding ideas
    - Includes votes
    - Models only for now

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100928165056) do
+ActiveRecord::Schema.define(:version => 20100930150423) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                             :default => "", :null => false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(:version => 20100928165056) do
     t.datetime "date"
     t.string   "description"
     t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ideas", :force => true do |t|
+    t.string   "description"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -90,5 +97,12 @@ ActiveRecord::Schema.define(:version => 20100928165056) do
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.integer  "idea_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
