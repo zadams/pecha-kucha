ec3c198441b90d57db745baf23c7caef2a65536b
Zack Adams <zack@pair7.(none)>
2010-10-18 14:15:49 -0400

    [JS/ZA] Config change for Devise mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,5 +12,8 @@ config.action_view.cache_template_loading            = true
 
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.raise_delivery_errors = false
 ActionMailer::Base.default_charset = "iso-8859-1"
+
+config.action_mailer.default_url_options = { :host => 'pecha-kucha.nor.primedia.com' }
+
